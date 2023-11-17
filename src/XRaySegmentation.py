6688081253cc5dc85109727cfc428e2b864e0162
gloriastucchi <stucchi.gloria@gmail.com>
2023-11-17 15:59:52 +0100

    new loss

--- src/XRaySegmentation.py
+++ src/XRaySegmentation.py
@@ -12,6 +12,7 @@ from UNet_3Plus import UNet_3Plus
 import torch.nn.functional as F
 from dice_loss import *
 import dice_loss as dl
+import random
 
 
 # HYPERPARAMETERS
@@ -189,6 +190,7 @@ model = UNet_3Plus(in_channels=1, n_classes=3).to(device)
 
 # Use DiceLoss, based on the concept of IoU
 loss_fn = dl.DiceLoss()
+#loss_fn = dl.IOU_loss()
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
 step = 0
@@ -199,7 +201,7 @@ valid_accuracies = []
 train_losses = []
 valid_losses = []
 
-current_time = time.time()
+#current_time = time.time()
 
 for epoch in range(NUM_EPOCHS):
     train_accuracies_batches = []
@@ -256,6 +258,7 @@ for epoch in range(NUM_EPOCHS):
 
             print(f"Step {step:<5}   training accuracy: {train_accuracies[-1]}")
             print(f"             validation accuracy: {valid_accuracies[-1]}")
+            print(f"             dice loss over the three classes: {loss}")
 
 
 if TESTING:
@@ -278,6 +281,6 @@ if TESTING:
     print(f"Test accuracy: {test_accuracy}")
 
 
-current_time = time.time() - current_time
-print(f"Finished training. Took {current_time/60} min")
+#current_time = time.time() - current_time
+#print(f"Finished training. Took {current_time/60} min")
 plot_train_val_loss_and_accuarcy(train_losses, valid_losses, train_accuracies, valid_accuracies)
